f012155ef902b1e4708bcb057d7eeefe188dc038
rstuven <rstuven@74c0767a-7647-11de-a180-a5af1b7afe95>
2009-07-25 23:24:05 +0000

    Code documentation.

--- trunk/src/LastHorizonte.Launcher/Program.cs
+++ trunk/src/LastHorizonte.Launcher/Program.cs
@@ -8,7 +8,13 @@ namespace LastHorizonte.Launcher
 		{
 			do
 			{
+				// Reset exit code.
+				// In this app, exit code 2 means "Restart"
 				Environment.ExitCode = 0;
+
+				// Start app in a new domain that use shadow copy files.
+				// Shadow copies allow autoupdater to overwrite assemblies
+				// while the app is running.
 				var setup = new AppDomainSetup();
 				setup.ShadowCopyFiles = "true";
 				var domain = AppDomain.CreateDomain("", AppDomain.CurrentDomain.Evidence, setup);
